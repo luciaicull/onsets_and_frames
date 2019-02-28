b5107e5bb331c9685dd84854b80c271e8efca9d6
luciaicull <lj2141301@gmail.com>
2019-02-28 20:36:34 +0900

    modified constants.py

--- constants.py
+++ constants.py
@@ -1,3 +1,10 @@
 import torch
 
 SAMPLE_RATE = 16000
+HOP_LENGTH = 512
+MELS_BIN = 229
+FFT_WINDOW = 2048
+
+ONSET_FRAME_THRESHOLD = 0.5
+
+DEFAULT_DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
