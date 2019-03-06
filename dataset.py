e4bdde51af12b1c57051e18a21fc3c59c403ddb1
luciaicull <lj2141301@gmail.com>
2019-03-07 00:11:36 +0900

    first commit

--- dataset.py
+++ dataset.py
@@ -2,3 +2,15 @@ import numpy as np
 from torch.utils.data import Dataset
 
 from .constants import *
+
+class AudioToPianoRollDataset(Dataset):
+    def __init__(self, path, device=DEFAULT_DEVICE):
+        self.path = path
+        self.device = device
+
+        self.data = []
+        print('Loading %d group%s of %s at %s' % (len(groups), 's'[:len(groups) - 1], self.__class__.__name__, path))
+
+
+    def __getitem__(self, index):
+
