f2c231ad919e776f45cedf1b674a8c2c62761ccf
yoojin <lj2141301@gmail.com>
2019-03-28 02:11:51 +0900

    20190327

--- dataset.py
+++ dataset.py
@@ -3,15 +3,28 @@ from torch.utils.data import Dataset
 
 from .constants import *
 
-class AudioToPianoRollDataset(Dataset):
-    def __init__(self, path, device=DEFAULT_DEVICE):
+class PianoRollAudioDataset(Dataset):
+    def __init__(self, path):
         self.path = path
-        self.device = device
 
         self.data = []
-        # print('Loading %d group%s of %s at %s' % (len(groups), 's'[:len(groups) - 1], self.__class__.__name__, path))
+
+    def __getitem__(self, index):
+        data = self.data[index]
+        result = dict(path=data['path'])
+
+    def __len__(self):
+        return len(self.data)
+
+class MAESTRO(PianoRollAudioDataset):
+    def __init__(self, path='/ssd2/maestro/maestro-v1.0.0'):
+        super().__init__(path)
+
+
+class MAPS(PianoRollAudioDataset):
+    def __init__(self, path='/ssd2/MAPS_MUS'):
+        super().__init__(path)
 
 
 
-#    def __getitem__(self, index):
 
